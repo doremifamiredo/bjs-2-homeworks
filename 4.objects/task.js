4ec4210ba8a6f1f2aedbd07ea7a3950043ace782
Oleg Somov <doremi@fasollasi.ru>
2024-11-03 19:29:15 +0300

    The fourth task is complete

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) console.log("Student has been expelled.");
+	if (this.excluded) return;
+	if (!!marks.length) this.marks.push(...marks);
+	return;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) return 0;
+	let sumMarks = this.marks.reduce((acc, cur) => acc + cur);
+	return sumMarks / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
